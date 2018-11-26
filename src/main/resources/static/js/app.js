530c43d8d4e6ba3ae99c6b40b6f23d8a974aa4c1
Ruslan A. Zarechnev <zarechnev.r@gmail.com>
2018-11-26 21:03:11 +0700

    FrontEnd ready.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,1 +1,23 @@
 $(document).foundation()
+
+function getMessages() {
+    $("#chat").load("get_messages/");
+}
+
+function addMsg() {
+    var userName = $("#user_name").val();
+    var Msg = $("#user_message").val();
+
+    $.get( "add_message/", { name: userName, message: Msg } )
+        .done(function() {
+            getMessages();
+        });
+}
+
+$("#send_msg_button").click(function() {
+    addMsg();
+});
+
+getMessages();
+
+setInterval(getMessages, 3000);
